7eff4c1dd661ec8955dc42f608123344cbf1309c
Richard Decal <richard.decal@ncf.edu>
2019-06-04 19:53:37 -0300

    more cookiecutter scaffolding

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=' a 10-bit encoding standard for macros with fault tolerance ',
+    author='Richard Decal',
+    license='',
+)
