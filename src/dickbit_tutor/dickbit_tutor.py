0946b8b9019747226a97142e068053f8ba885913
Richard Decal <richard.decal@ncf.edu>
2019-10-06 19:54:37 -0300

    boilterplate

--- src/dickbit_tutor/dickbit_tutor.py
+++ src/dickbit_tutor/dickbit_tutor.py
@@ -6,3 +6,52 @@ decide on mapping from 10-fingers to letters
 """
 
 
+import dash
+import dash_daq as daq
+import dash_html_components as html
+import keyboard
+
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    daq.Indicator(
+        id='my-indicator',
+        label="L5",
+    ),
+    daq.Indicator(
+        id='my-indicator',
+        label="L4",
+    ),
+    daq.Indicator(
+        id='my-indicator',
+        label="L3",
+    ),
+    daq.Indicator(
+        id='my-indicator',
+        label="L2",
+    ),
+    daq.Indicator(
+        id='my-indicator',
+        label="L1",
+    )
+])
+
+
+@app.callback(
+    dash.dependencies.Output('my-indicator', 'value'),
+    [dash.dependencies.Input('my-indicator-button', 'n_clicks')]
+)
+def update_output(value):
+    if value % 2 is 0:
+        value = True
+    else:
+        value = False
+    return value
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
